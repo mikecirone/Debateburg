440c38ace4077a9e2b36a5e627c4aad89e908b59
mikecirone <mikecirone@gmail.com>
2017-04-24 00:06:37 -0700

    got rid of spaghetti code in userReducer.js

--- client/user/userReducer.jsx
+++ client/user/userReducer.jsx
@@ -1,43 +1,17 @@
 import * as actionTypes from 'actionTypes';
 
 function userReducer(state = {}, action) {
-    return {
-        authToken: ((state = "", action) => {
-                    switch (action.type) {
-                        case actionTypes.FETCH_REGISTER_SUCCESS:
-                        case actionTypes.FETCH_LOGIN_SUCCESS:
-                            return action.data.authToken;
-                        default:
-                            return state;
-                    }
-                  })(state.authToken, action),
-        email: ((state = "", action) => {
-                    switch (action.type) {
-                        case actionTypes.FETCH_REGISTER_SUCCESS:
-                        case actionTypes.FETCH_LOGIN_SUCCESS:
-                            return action.data.email;
-                        default:
-                            return state;
-                    }
-                })(state.email, action),
-        username: ((state = "", action) => {
-                    switch (action.type) {
-                        case actionTypes.FETCH_REGISTER_SUCCESS:
-                        case actionTypes.FETCH_LOGIN_SUCCESS:
-                            return action.data.username;
-                        default:
-                            return state;
-                    }
-                })(state.username, action),
-        _id: ((state = "", action) => {
-                  switch (action.type) {
-                      case actionTypes.FETCH_REGISTER_SUCCESS:
-                      case actionTypes.FETCH_LOGIN_SUCCESS:
-                          return action.data._id;
-                      default:
-                          return state;
-                  }
-              })(state._id, action)
+    switch(action.type) {
+      case actionTypes.FETCH_REGISTER_SUCCESS:
+      case actionTypes.FETCH_LOGIN_SUCCESS:
+          return {
+            authToken: action.data.authToken,
+            email: action.data.email,
+            username: action.data.username,
+            _id: action.data._id
+          };
+      default:
+        return state
     }
 }
 
